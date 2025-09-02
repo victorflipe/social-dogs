26e9cf8445b003274a561f72e3f00172c4805ea4
Victor Felipe Oliveira <victorf.roliver@gmail.com>
2025-09-02 15:58:50 -0300

    fix basename of the project

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className='App'>
-      <BrowserRouter>
+      <BrowserRouter basename='/social-dogs'>
         <UserStorage>
           <Header />
           <main className='AppBody'>
