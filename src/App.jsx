8131f6088a4c6af7cf0e98694df319ea26abed81
Victor Felipe Oliveira <victorf.roliver@gmail.com>
2025-09-02 16:29:34 -0300

    return previous config

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className='App'>
-      <BrowserRouter basename='/social-dogs'>
+      <BrowserRouter>
         <UserStorage>
           <Header />
           <main className='AppBody'>
